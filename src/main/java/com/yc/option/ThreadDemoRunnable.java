1920bf09f9dc40c8ca15c86291318ecb841f97f0
chungyan <59307416+chungyan@users.noreply.github.com>
2020-06-30 16:26:45 +0800

    Update ThreadDemoRunnable.java

--- src/main/java/com/yc/option/ThreadDemoRunnable.java
+++ src/main/java/com/yc/option/ThreadDemoRunnable.java
@@ -9,7 +9,7 @@ public class ThreadDemoRunnable implements Runnable{
     public void run() {
 
         for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName());
+            System.out.println(Thread.currentThread().getName()+"ing");
         }
     }
 }
